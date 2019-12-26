8b4999d392198c906e62e607fca4acb5aaf07d3d
sam <samwhite@uw.edu>
2019-12-26 14:09:59 -0800

    add set -e command to kill script if any command fails

--- sbatch_scripts/20191225_cbai_trinotate.sh
+++ sbatch_scripts/20191225_cbai_trinotate.sh
@@ -17,6 +17,9 @@
 ## Specify the working directory for this job
 #SBATCH --chdir=/gscratch/scrubbed/samwhite/outputs/20191225_cbai_trinotate
 
+# Exit script if any command fails
+set -e
+
 # Load Python Mox module for Python module availability
 
 module load intel-python3_2017
