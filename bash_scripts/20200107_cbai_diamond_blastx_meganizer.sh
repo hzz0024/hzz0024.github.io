06c341a211f5b9c212ede338821e6456a16516f8
sam <samwhite@uw.edu>
2020-01-17 21:59:30 -0800

    add daa concatenation loop

--- bash_scripts/20200107_cbai_diamond_blastx_meganizer.sh
+++ bash_scripts/20200107_cbai_diamond_blastx_meganizer.sh
@@ -48,6 +48,14 @@ do
 done
 
 # Concatenate R1 and R2 DAA files
+for index in "${!daa_array_R1[@]}"
+do
+  sample_name=$(echo "${daa_array_R1[index]}" | awk -F "_" '{print $1}')
+  {
+    cat ${daa_array_R1[index]}
+    cat ${daa_array_R2[index]}
+  } >> ${sample_name}.blastx.cat.daa
+done
 
 ## Run MEGANIZER
 
